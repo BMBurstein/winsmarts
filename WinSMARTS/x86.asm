2c02a6ef3d7b7e4f5b54db75db106d1d4e901ecd
Baruch Burstein <bmburstein@gmail.com>
2012-12-03 10:19:29 +0200

    x86 context switch MASM added

---
+++ WinSMARTS/x86.asm
@@ -0,0 +1,35 @@
+; Copyright (c) Microsoft Corporation.  All rights reserved.
+
+.686
+.model flat, c
+
+.code
+
+contextSwitch PROC EXPORT
+	pushfd
+	pushad
+	mov eax, [esp+28h]
+	mov ebx, [esp+2ch]
+	mov [eax], esp
+	mov esp, ebx
+	popad
+	popfd
+	ret
+contextSwitch ENDP
+
+newTask PROC EXPORT
+	mov ebx, esp
+	mov esp, [esp+0ch]
+	push [ebx+14h] ; retParam
+	push eax ; dummy - DO NOT RETURN FROM RET
+	push [ebx+08h] ; fnParam
+	push [ebx+10h] ; ret
+	push [ebx+04h] ; fn
+	pushfd
+	pushad
+	mov eax, esp
+	mov esp, ebx
+	ret
+newTask ENDP
+
+end
