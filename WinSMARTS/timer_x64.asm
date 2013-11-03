f0d5800a3b8b74bb07856427f4f64d1a4df07ad7
Elad Yehiel <elad55@gmail.com>
2013-11-03 16:30:28 +0200

    Implementation of: newTask, doTimerAsm

--- WinSMARTS/timer_x64.asm
+++ WinSMARTS/timer_x64.asm
@@ -43,23 +43,14 @@ endm
 doTimerAsm PROC EXPORT
 	pushM
 	mov rax, [rsp+80h]
-	push QWORD PTR [rsp+88h]
+	;push QWORD PTR [rsp+88h]
+	mov rcx, [rsp+88h]
+	sub rsp,20h
 	call rax
+	add rsp,20h
 	popM
 	add rsp, 10h
 	ret
 doTimerAsm ENDP
-
-;doTimerAsm PROC EXPORT
-;	pushfd             ; backup flags register (4 Bytes)
-;	pushad             ; bakcup general purpose registers (8*4 Bytes)
-;	mov eax, [esp+24h] ; load interrupt handler pointer
-;	push DWORD PTR [esp+28h]     ; push handler parameter
-;	call eax           ; invoke interrupt handler
-;	popad              ; after return from interrupt handler, restore general purpose registers
-;	popfd              ; restore flags register
-;	add esp, 08h       ; pop interrupt handler pointer and the parameter
-;	ret                ; resume
-;doTimerAsm ENDP
 
 end
