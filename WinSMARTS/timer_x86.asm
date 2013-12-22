197c99708479e591ab525c4789664bf9898debf5
Elad Yehiel <elad55@gmail.com>
2013-12-23 00:53:50 +0200

    Proofread comments

--- WinSMARTS/timer_x86.asm
+++ WinSMARTS/timer_x86.asm
@@ -11,7 +11,7 @@ doTimerAsm PROC EXPORT
 	pushfd             ; backup flags register (4 Bytes)
 	pushad             ; bakcup general purpose registers (8*4 Bytes)
 	mov eax, [ebp+08h] ; load interrupt handler pointer
-	push DWORD PTR [ebp+0Ch]     ; push handler parameter
+	push DWORD PTR [ebp+0Ch]     ; push interrupt handler parameter
 	call eax           ; invoke interrupt handler
 	popad              ; after return from interrupt handler, restore general purpose registers
 	popfd              ; restore flags register
