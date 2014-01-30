8201205f405f8eadc652e3e8153d21681ce826b3
Baruch Burstein <bmburstein@gmail.com>
2014-01-30 10:55:35 +0200

    Cleanup

--- WinSMARTS/Philosophe.cpp
+++ WinSMARTS/Philosophe.cpp
@@ -1,6 +1,6 @@
-//#include <iostream>
-//using namespace std;
 #include "Philosophe.h"
+#include <iostream>
+using namespace std;
 
 Philosophe::Philosophe(int id_, MyMonitor* myMonitor_, int thinkingTime_, int eatingTime_)
 	:id(id_),
@@ -15,19 +15,19 @@ void Philosophe::run(WinSMARTS* SMARTS_)
 	while (true)
 	{
 		SMARTS->contextSwitchOff();
-		//cout << "Philosophe " << id << "thinking" << endl;
+		cout << "Philosophe " << id << "thinking" << endl;
 		SMARTS->contextSwitchOn();
 		
 		SMARTS->sleep(thinkingTime*500); //thinking
 		
 		SMARTS->contextSwitchOff();
-		//cout << "Philosophe " << id << "hungry" << endl;
+		cout << "Philosophe " << id << "hungry  " << endl;
 		SMARTS->contextSwitchOn();
 		
 		myMonitor->pickUp(id);
 		
 		SMARTS->contextSwitchOff();
-		//cout << "Philosophe " << id << "eating" << endl;
+		cout << "Philosophe " << id << "eating  " << endl;
 		SMARTS->contextSwitchOn();
 		
 		busyWait(eatingTime); //eating
